ce408a68de19c03704e4259aed17da53bce73d69
Gabriel Maximiniano <gabmaxsan@gmail.com>
2020-12-22 11:13:59 -0300

    add profile user relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -61,4 +61,8 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function profile() {
+        return $this->hasOne(Profile::class);
+    }
 }
