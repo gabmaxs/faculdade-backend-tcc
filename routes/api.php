f9e058de97c4a89fbb5a35ed48c17751a34cdeb6
Gabriel Maximiniano <gabmaxsan@gmail.com>
2020-12-22 17:28:26 -0300

    fix profile route

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
-    Route::post('login', 'AuthController@login');
+    Route::post('login', 'AuthController@login')->name("login");
     Route::post('register', 'AuthController@register');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
@@ -23,5 +23,5 @@ Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
 });
 
 Route::middleware(['auth:api'])->group(function () {
-    Route::post('user/{user}/profile', 'ProfileController@store');
+    Route::post('user/profile', 'ProfileController@store');
 });
