e100f7befc9216828cafb6896986a3500cdf309c
Gabriel Maximiniano <gabmaxsan@gmail.com>
2020-12-22 11:34:28 -0300

    add routes

--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
     Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
+});
+
+Route::middleware(['auth:api'])->group(function () {
+    Route::post('user/{user}/profile', 'ProfileController@store');
 });
