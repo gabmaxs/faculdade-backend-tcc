d903d5a10b866718094e989413406da6754514ad
Gabriel Maximiniano <gabmaxsan@gmail.com>
2020-12-22 11:13:03 -0300

    add register method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -21,15 +24,73 @@ class AuthController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function login()
+    public function login(Request $request)
     {
-        $credentials = request(['email', 'password']);
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|max:255',
+            'password' => 'required|string|max:255',
+        ]);
 
-        if (! $token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => false,
+                "payload" => [
+                    "message" => $validator->errors()
+                ],
+                "error" => [
+                    "code" => 400,
+                    "message" => "Bad Request"
+                ]
+            ], 400);
         }
 
-        return $this->respondWithToken($token);
+        $credentials = $request->only('email', 'password');
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json([
+                "success" => false,
+                "payload" => [
+                    "message" => "E-mail ou senha inválidos"
+                ],
+                "error" => [
+                    "code" => 401,
+                    "message" => "Unauthorized"
+                ]
+            ], 401);
+        }
+
+        return $this->respondWithToken($token, 200);
+    }
+
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max:255|unique:users',
+            'password' => 'required|string|max:255|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => false,
+                "payload" => [
+                    "message" => $validator->errors()
+                ],
+                "error" => [
+                    "code" => 400,
+                    "message" => "Bad Request"
+                ]
+            ], 400);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = auth()->login($user);
+
+        return $this->respondWithToken($token, 201);
     }
 
     /**
@@ -51,7 +112,19 @@ class AuthController extends Controller
     {
         auth()->logout();
 
-        return response()->json(['message' => 'Successfully logged out']);
+        return response()->json([
+            "success" => true,
+            "payload" => [
+                'message' => 'Desconectado com sucesso'
+            ],
+            "links" => [
+                [
+                    "href" => "",
+                    "rel" => "",
+                    "type" => ""
+                ],
+            ]
+        ], 200);
     }
 
     /**
@@ -61,7 +134,7 @@ class AuthController extends Controller
      */
     public function refresh()
     {
-        return $this->respondWithToken(auth()->refresh());
+        return $this->respondWithToken(auth()->refresh(), 200);
     }
 
     /**
@@ -71,12 +144,22 @@ class AuthController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    protected function respondWithToken($token)
+    protected function respondWithToken($token, $code)
     {
         return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
-        ]);
+            "success" => true,
+            "payload" => [
+                'access_token' => $token,
+                'token_type' => 'bearer',
+                'expires_in' => auth()->factory()->getTTL() * 60
+            ],
+            "links" => [
+                [
+                    "href" => "",
+                    "rel" => "",
+                    "type" => ""
+                ],
+            ]
+        ], $code);
     }
 }
