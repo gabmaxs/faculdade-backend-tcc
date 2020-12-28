265d25a9dea0a2378b890922eb15c7494f93a56e
Gabriel Maximiniano <gabmaxsan@gmail.com>
2020-12-28 13:47:26 -0300

    add recipe controller

---
+++ app/Http/Controllers/RecipeController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Recipe;
+use App\Models\Ingredient;
+use Illuminate\Support\Facades\Validator;
+
+class RecipeController extends Controller
+{
+    public function store(Request $request) {
+        $user = auth()->user();
+
+        $validator = Validator::make($request->all(), [
+            "name" => "required|string|max:255",
+            "image" => "nullable|string|max:255",
+            "number_of_servings" => "required|numeric|max:50",
+            "cooking_time" => "required|numeric|max:360",
+            "how_to_cook" => "required|array|max:2000",
+            "category_id" => "required|numeric",
+            "list_of_ingredients" => "required|array|max:2000",
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => false,
+                "payload" => [
+                    "message" => $validator->errors()
+                ],
+                "error" => [
+                    "code" => 400,
+                    "message" => "Bad Request"
+                ]
+            ], 400);
+        }
+
+        $recipe = $user->recipes()->create([
+            "name" => $request->name,
+            "image" => $request->image,
+            "number_of_servings" => $request->number_of_servings,
+            "cooking_time" => $request->cooking_time,
+            "how_to_cook" => $request->how_to_cook,
+            "category_id" => $request->category_id,
+        ]);
+
+        foreach($request->get('list_of_ingredients') as $ingredient_array) {
+            $ingredient = Ingredient::firstOrCreate([
+                "name" => ucfirst($ingredient_array["name"])
+            ]);
+            $recipe->ingredients()->attach($ingredient->id, [
+                "quantity" => $ingredient_array["quantity"],
+                "measure" => $ingredient_array["measure"]
+            ]);
+        }
+
+        return response()->json([
+            "success" => true,
+            "payload" => [
+                'message' => 'Receita criada com sucesso',
+                'data' => $recipe->with("Ingredients")->get()
+            ],
+            "links" => [
+                [
+                    "href" => "",
+                    "rel" => "",
+                    "type" => ""
+                ],
+            ]
+        ], 201);
+    }
+}
--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -13,8 +13,12 @@ class Recipe extends Model
         "name", "image", "number_of_servings", "cooking_time", "how_to_cook", "category_id", "user_id"
     ];
 
+    protected $casts = [
+        "how_to_cook" => "array"
+    ];
+
     public function ingredients() {
-        return $this->belongsToMany(Ingredient::class);
+        return $this->belongsToMany(Ingredient::class)->withPivot("quantity","measure")->as("details");
     }
     
     public function category() {
