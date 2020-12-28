1c0ad698fd2a9532ea8fd8edc4c26a0fe6276f32
Gabriel Maximiniano <gabmaxsan@gmail.com>
2020-12-28 13:47:44 -0300

    add recipe and category routes

--- routes/api.php
+++ routes/api.php
@@ -14,14 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
-    Route::post('login', 'AuthController@login')->name("login");
-    Route::post('register', 'AuthController@register');
-    Route::post('logout', 'AuthController@logout');
-    Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
+Route::group(["middleware" => "api", "prefix" => "auth"], function ($router) {
+    Route::post("login", "AuthController@login")->name("login");
+    Route::post("register", "AuthController@register");
+    Route::post("logout", "AuthController@logout");
+    Route::post("refresh", "AuthController@refresh");
+    Route::post("me", "AuthController@me");
 });
 
-Route::middleware(['auth:api'])->group(function () {
-    Route::post('user/profile', 'ProfileController@store');
+Route::middleware(["auth:api"])->group(function () {
+    Route::post("user/profile", "ProfileController@store");
+
+    Route::get("category", "CategoryController@index");
+    
+    Route::post("recipe", "RecipeController@store");
 });
