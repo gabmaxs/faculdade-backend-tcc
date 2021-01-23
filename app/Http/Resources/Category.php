ced5704c6a2128de661a008e506cd11db3c83096
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-01-23 11:45:02 -0300

    fix(category-resource): add with method in categoryResource

--- app/Http/Resources/Category.php
+++ app/Http/Resources/Category.php
@@ -6,6 +6,14 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class Category extends JsonResource
 {
+    private $text;
+    public function __construct($obj, $text)
+    {
+        parent::__construct($obj);
+        $this->text = $text;
+
+    }
+    
     /**
      * Transform the resource into an array.
      *
@@ -17,6 +25,13 @@ class Category extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+        ];
+    }
+
+    public function with($request) {
+        return [
+            "success" => true,
+            "message" => $this->text
         ];
     }
 }
