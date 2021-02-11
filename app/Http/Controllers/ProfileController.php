d8f33861820f48207a1f1ab95f2ef7c7cac63826
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-02-11 12:10:15 -0300

    refactor(profile-controller): use new features in store method

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,7 +6,6 @@ use App\Facade\Response;
 use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 use App\Http\Resources\User as UserResource;
 
 class ProfileController extends Controller
@@ -22,23 +21,18 @@ class ProfileController extends Controller
     {
         $user = auth()->user();
 
-        $validator = Validator::make($request->all(), [
+        $request->validate([
             'culinary_level' => 'required|max:10|numeric',
             'gender' => 'required|string|max:255',
             'photo' => 'nullable',
         ]);
-
-        if ($validator->fails()) {
-            return response()->json(Response::error($validator->errors()), 400);
-        }
 
         $user->profile()->create([
             "culinary_level" => $request->culinary_level,
             "gender" => $request->gender
         ]);
 
-        $data = $user->with("Profile")->where('id',$user->id)->get();
-        return response()->json(Response::success($data, 'Perfil criado com sucesso'),201);
+        return (new UserResource($user, "Perfil criado com sucesso"))->response()->setStatusCode(201);
     }
 
     /**
