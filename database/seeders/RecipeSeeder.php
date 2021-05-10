52216a74058bc44c3277e59885ebe4467b88ca7d
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-05-10 14:28:16 -0300

    add more recipes

--- database/seeders/RecipeSeeder.php
+++ database/seeders/RecipeSeeder.php
@@ -387,7 +387,7 @@ class RecipeSeeder extends Seeder
                     "Misture por volta de 3 minutos até ficar uma mistura homogênea",
                     "Retire do fogo e acrescente a cebolinha"
                 ],
-                "category_id" => 9,
+                "category_id" => 11,
                 "user_id" => 1,
                 "ingredients" => [
                     [
@@ -414,6 +414,326 @@ class RecipeSeeder extends Seeder
                         "name" => "Sal",
                         "quantity" => "0.5",
                         "measure" => "colher de sobremesa"
+                    ],
+                ],
+            ],
+            [
+                "name" => "Arroz branco",
+                "image" => env("STORAGE_URL")."/public%2Frecipes%2F{image}?alt=media",
+                "number_of_servings" => 4,
+                "cooking_time" => 20,
+                "how_to_cook" => [
+                    "Em uma panela, aqueça o óleo e frite o alho",
+                    "Adicione o arroz e mexa até ficar brilhante",
+                    "Coloque 3 xícaras de água fervente e acrescente o sal e mistute",
+                    "Cozinhe em fogo baixo até a água secar completamente"
+                ],
+                "category_id" => 11,
+                "user_id" => 1,
+                "ingredients" => [
+                    [
+                        "name" => "Óleo",
+                        "quantity" => "1",
+                        "measure" => "colher de sopa"
+                    ],
+                    [
+                        "name" => "dente de alho",
+                        "quantity" => "1",
+                        "measure" => "unidade"
+                    ],
+                    [
+                        "name" => "Arroz",
+                        "quantity" => "1.5",
+                        "measure" => "xícara de chá"
+                    ],
+                    [
+                        "name" => "Sal",
+                        "quantity" => "0.5",
+                        "measure" => "colher de sopa"
+                    ],
+                    [
+                        "name" => "Água",
+                        "quantity" => "3",
+                        "measure" => "xícara de chá"
+                    ],
+                ],
+            ],
+            [
+                "name" => "Feijão tropeiro",
+                "image" => env("STORAGE_URL")."/public%2Frecipes%2F{image}?alt=media",
+                "number_of_servings" => 4,
+                "cooking_time" => 40,
+                "how_to_cook" => [],
+                "category_id" => 11,
+                "user_id" => 1,
+                "ingredients" => [
+                    [
+                        "name" => "farinha de mandioca",
+                        "quantity" => "1",
+                        "measure" => "xícara de chá"
+                    ],
+                    [
+                        "name" => "bacon",
+                        "quantity" => "250",
+                        "measure" => "gramas"
+                    ],
+                    [
+                        "name" => "calabresa",
+                        "quantity" => "300",
+                        "measure" => "gramas"
+                    ],
+                    [
+                        "name" => "cebola",
+                        "quantity" => "0.5",
+                        "measure" => "unidade"
+                    ],
+                    [
+                        "name" => "cebolinha",
+                        "quantity" => "2",
+                        "measure" => "colher de sopa"
+                    ],
+                    [
+                        "name" => "ovo",
+                        "quantity" => "3",
+                        "measure" => "unidade"
+                    ],
+                    [
+                        "name" => "feijão cozido",
+                        "quantity" => "3",
+                        "measure" => "xícaras de chá"
+                    ],
+                    [
+                        "name" => "óleo",
+                        "quantity" => "2",
+                        "measure" => "colher de sopa"
+                    ],
+                ],
+            ],
+            [
+                "name" => "Pudim de leite condensado",
+                "image" => env("STORAGE_URL")."/public%2Frecipes%2F{image}?alt=media",
+                "number_of_servings" => 8,
+                "cooking_time" => 60,
+                "how_to_cook" => [
+                    "Prepare a calda com o açúcar",
+                    "Hidrate a gelatina conforme a embalagem e dissolva-a no leite",
+                    "Coloque o leite condensado, o creme de leite, o leite com a gelatina e bata bem até formar uma mistura homogênea",
+                    "Despeje a mistura na forma e leve para a geladeira por 4 horas",
+                    "Desenform em um prato"
+                ],
+                "category_id" => 5,
+                "user_id" => 1,
+                "ingredients" => [
+                    [
+                        "name" => "leite condensado",
+                        "quantity" => "1",
+                        "measure" => "unidade"
+                    ],
+                    [
+                        "name" => "creme de leite",
+                        "quantity" => "1",
+                        "measure" => "unidade"
+                    ],
+                    [
+                        "name" => "leite",
+                        "quantity" => "1",
+                        "measure" => "xícara de chá"
+                    ],
+                    [
+                        "name" => "gelatina sem sabor",
+                        "quantity" => "1",
+                        "measure" => "unidade"
+                    ],
+                    [
+                        "name" => "acúcar",
+                        "quantity" => "3",
+                        "measure" => "colher de sopa"
+                    ],
+                ],
+            ],
+            [
+                "name" => "Torta de bis",
+                "image" => env("STORAGE_URL")."/public%2Frecipes%2F{image}?alt=media",
+                "number_of_servings" => 10,
+                "cooking_time" => 40,
+                "how_to_cook" => [
+                    "Misture o creme de leite e o doce de leite e coloque em uma travessa",
+                    "Triture os bis e coloque por cima",
+                    "Leve a torta na geladeira por 2 horas"
+                ],
+                "category_id" => 3,
+                "user_id" => 1,
+                "ingredients" => [
+                    [
+                        "name" => "doce de leite",
+                        "quantity" => "500",
+                        "measure" => "gramas"
+                    ],
+                    [
+                        "name" => "creme de leite",
+                        "quantity" => "4",
+                        "measure" => "unidade"
+                    ],
+                    [
+                        "name" => "caixa de bis",
+                        "quantity" => "2",
+                        "measure" => "unidade"
+                    ],
+                ],
+            ],
+            [
+                "name" => "Molho pesto",
+                "image" => env("STORAGE_URL")."/public%2Frecipes%2F{image}?alt=media",
+                "number_of_servings" => 20,
+                "cooking_time" => 20,
+                "how_to_cook" => [],
+                "category_id" => 9,
+                "user_id" => 1,
+                "ingredients" => [
+                    [
+                        "name" => "manjericão",
+                        "quantity" => "2",
+                        "measure" => "xícara de chá"
+                    ],
+                    [
+                        "name" => "noz",
+                        "quantity" => "3",
+                        "measure" => "unidade"
+                    ],
+                    [
+                        "name" => "queijo parmesão",
+                        "quantity" => "1",
+                        "measure" => "xícara de chá"
+                    ],
+                    [
+                        "name" => "dente de alho",
+                        "quantity" => "2",
+                        "measure" => "unidade"
+                    ],
+                    [
+                        "name" => "azeite",
+                        "quantity" => "1.5",
+                        "measure" => "xícara de chá"
+                    ],
+                    [
+                        "name" => "Sal",
+                        "quantity" => "0",
+                        "measure" => "unidade"
+                    ],
+                ],
+            ],
+            [
+                "name" => "Molho branco",
+                "image" => env("STORAGE_URL")."/public%2Frecipes%2F{image}?alt=media",
+                "number_of_servings" => 4,
+                "cooking_time" => 15,
+                "how_to_cook" => [
+                    "Dissolva a farinha de trigo em meia xícara de leite e reserve",
+                    "Coloque em uma panela a manteiga, o alho e a cebola e deixe dourar",
+                    "Adicione o leite com a farinha e mexa",
+                    "Adicione o restante do leite, mexa até levantar fervura e engrossar"
+                ],
+                "category_id" => 9,
+                "user_id" => 1,
+                "ingredients" => [
+                    [
+                        "name" => "leite",
+                        "quantity" => "2",
+                        "measure" => "xícara de chá"
+                    ],
+                    [
+                        "name" => "manteiga",
+                        "quantity" => "2",
+                        "measure" => "colher de chá"
+                    ],
+                    [
+                        "name" => "dente de alho",
+                        "quantity" => "2",
+                        "measure" => "unidade"
+                    ],
+                    [
+                        "name" => "cebola",
+                        "quantity" => "0.5",
+                        "measure" => "unidade"
+                    ],
+                    [
+                        "name" => "farinha de trigo",
+                        "quantity" => "2",
+                        "measure" => "colher de sopa"
+                    ],
+                    [
+                        "name" => "Sal",
+                        "quantity" => "0",
+                        "measure" => "unidade"
+                    ],
+                ],
+            ],
+            [
+                "name" => "Pão de queijo",
+                "image" => env("STORAGE_URL")."/public%2Frecipes%2F{image}?alt=media",
+                "number_of_servings" => 18,
+                "cooking_time" => 50,
+                "how_to_cook" => [
+                    "Misture em uma tigela o polvilho azedo, o creme de leite e o queijo",
+                    "Adicione o sal e misture",
+                    "Enrole as bolinhas e coloque em um tabuleiro untado com óleo",
+                    "Pré-aqueça o forne e asse por 40 minutos"
+                ],
+                "category_id" => 6,
+                "user_id" => 1,
+                "ingredients" => [
+                    [
+                        "name" => "creme de leite",
+                        "quantity" => "1",
+                        "measure" => "unidade"
+                    ],
+                    [
+                        "name" => "queijo ralado",
+                        "quantity" => "1",
+                        "measure" => "xícara de chá"
+                    ],
+                    [
+                        "name" => "polvilho azedo",
+                        "quantity" => "1",
+                        "measure" => "xícara de chá"
+                    ],
+                    [
+                        "name" => "sal",
+                        "quantity" => "0.25",
+                        "measure" => "colher de chá"
+                    ],
+                ],
+            ],
+            [
+                "name" => "Suco de abacaxi, maçã e gengibre",
+                "image" => env("STORAGE_URL")."/public%2Frecipes%2F{image}?alt=media",
+                "number_of_servings" => 8,
+                "cooking_time" => 10,
+                "how_to_cook" => [
+                    "Bata todos os ingredientes no liquidificador"
+                ],
+                "category_id" => 2,
+                "user_id" => 1,
+                "ingredients" => [
+                    [
+                        "name" => "rodelas de abacaxi",
+                        "quantity" => "4",
+                        "measure" => "unidade"
+                    ],
+                    [
+                        "name" => "maça",
+                        "quantity" => "1",
+                        "measure" => "unidade"
+                    ],
+                    [
+                        "name" => "gengibre",
+                        "quantity" => "2",
+                        "measure" => "gramas"
+                    ],
+                    [
+                        "name" => "água",
+                        "quantity" => "1",
+                        "measure" => "litro"
                     ],
                 ],
             ]
