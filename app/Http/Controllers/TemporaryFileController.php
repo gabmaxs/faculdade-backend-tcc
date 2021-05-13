a763557d261955f8e3608cf8073a31f3b797075c
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-05-12 21:15:43 -0300

    change max size upload

--- app/Http/Controllers/TemporaryFileController.php
+++ app/Http/Controllers/TemporaryFileController.php
@@ -9,7 +9,7 @@ class TemporaryFileController extends Controller
 {
     public function upload(Request $request) {
         $request->validate([
-            "image" => "required|file|mimes:jpg,gif,png,jpeg",
+            "image" => "required|file|mimes:jpg,gif,png,jpeg|max:2040",
         ]);
 
         if($request->hasFile("image")) {
